b8f6c1d4e43ad0a346843c91578d83a1f8db295c
draganjordanovic <150361053+draganjordanovic@users.noreply.github.com>
2025-12-01 18:08:44 +0100

    Set up project and add gitignore

---
+++ roller-coaster/Main.cpp
@@ -0,0 +1,62 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include "Util.h"
+
+int endProgram(std::string message) {
+    std::cout << message << std::endl;
+    glfwTerminate();
+    return -1;
+}
+
+int main()
+{
+    // Inicijalizacija GLFW i postavljanje na verziju 3 sa programabilnim pajplajnom
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Formiranje prozora za prikaz sa datim dimenzijama i naslovom
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Vezba 1", NULL, NULL);
+    if (window == NULL) return endProgram("Prozor nije uspeo da se kreira.");
+    glfwMakeContextCurrent(window);
+
+    // Inicijalizacija GLEW
+    if (glewInit() != GLEW_OK) return endProgram("GLEW nije uspeo da se inicijalizuje.");
+
+    unsigned int basicShader = createShader("basic.vert", "basic.frag");
+
+    float vertices[] = {
+         0.0
+    };
+
+    // Inicijalizacija VAO i VBO, tipičnih struktura za čuvanje podataka o verteksima
+    unsigned int VAO;
+    unsigned int VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    glClearColor(0.3f, 0.1f, 0.6f, 1.0f); // Postavljanje boje pozadine
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT); // Bojenje pozadine
+
+        glUseProgram(basicShader); // Podešavanje da se crta koristeći dati šejder
+        glBindVertexArray(VAO); // Podešavanje da se crta koristeći date vertekse
+
+
+        glfwSwapBuffers(window); // Zamena bafera - prednji i zadnji bafer se menjaju kao štafeta; dok jedan procesuje, drugi se prikazuje.
+        glfwPollEvents(); // Sinhronizacija pristiglih događaja
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ roller-coaster/Util.cpp
@@ -0,0 +1,83 @@
+#include "Util.h";
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+unsigned int compileShader(GLenum type, const char* source)
+{
+
+    std::string content = "";
+    std::ifstream file(source);
+    std::stringstream ss;
+    if (file.is_open())
+    {
+        ss << file.rdbuf();
+        file.close();
+        std::cout << "Uspjesno procitao fajl sa putanje \"" << source << "\"!" << std::endl;
+    }
+    else {
+        ss << "";
+        std::cout << "Greska pri citanju fajla sa putanje \"" << source << "\"!" << std::endl;
+    }
+    std::string temp = ss.str();
+    const char* sourceCode = temp.c_str();
+
+    int shader = glCreateShader(type);
+
+    int success;
+    char infoLog[512];
+    glShaderSource(shader, 1, &sourceCode, NULL);
+    glCompileShader(shader);
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (success == GL_FALSE)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        if (type == GL_VERTEX_SHADER)
+            printf("VERTEX");
+        else if (type == GL_FRAGMENT_SHADER)
+            printf("FRAGMENT");
+        printf(" sejder ima gresku! Greska: \n");
+        printf(infoLog);
+    }
+    return shader;
+}
+unsigned int createShader(const char* vsSource, const char* fsSource)
+{
+
+    unsigned int program;
+    unsigned int vertexShader;
+    unsigned int fragmentShader;
+
+    program = glCreateProgram();
+
+    vertexShader = compileShader(GL_VERTEX_SHADER, vsSource);
+    fragmentShader = compileShader(GL_FRAGMENT_SHADER, fsSource);
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_VALIDATE_STATUS, &success);
+    if (success == GL_FALSE)
+    {
+        glGetShaderInfoLog(program, 512, NULL, infoLog);
+        std::cout << "Objedinjeni sejder ima gresku! Greska: \n";
+        std::cout << infoLog << std::endl;
+    }
+
+    glDetachShader(program, vertexShader);
+    glDeleteShader(vertexShader);
+    glDetachShader(program, fragmentShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
